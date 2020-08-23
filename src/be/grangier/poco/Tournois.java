d26429f100a777d69fd494110c45602072657eeb
gladys.grangier@condorcet.be <gladys.grangier@condorcet.be>
2020-08-23 20:08:38 +0200

    Ajout des attributs + getter/setter+ methode classe Tournois

--- src/be/grangier/poco/Tournois.java
+++ src/be/grangier/poco/Tournois.java
@@ -1,5 +1,63 @@
 package be.grangier.poco;
 
-public class Tournois {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
+public class Tournois {
+	private String nom;
+	private String tableautype[] = {"messieurs", "dames","doubleMessieurs" , "doubleDames","doubleMixte" };
+	private ArrayList<Ordonnancement> ao = new ArrayList<Ordonnancement>();
+	
+	
+	public Tournois(String nom)
+	{	
+		this.nom = nom;
+		
+		for(int i = 0 ; i <getTableauChaine().length;i++)
+		{
+			
+			if(getTableauChaine()[i].contains("messieurs")) // 12
+    		{
+				ao.add(new Ordonnancement(getTableauChaine()[i],3));
+    		}
+			else
+			{
+				ao.add(new Ordonnancement(getTableauChaine()[i],2));
+			}
+		}
+	}
+	
+	public ArrayList<Ordonnancement> getAo() 
+	{
+		return ao;
+	}
+	
+
+	
+	public String getNom() 
+	{
+		return nom;
+	}
+	public void setNom(String nom)
+	{
+		this.nom = nom;
+	}
+	public String[] getTableauChaine() 
+	{
+		return tableautype;
+	}
+	public void setTableauChaine(String[] tableautype) 
+	{
+		this.tableautype = tableautype;
+	}
+	
+	
+	public void startAllBrackets() 
+	{
+		for(Ordonnancement o : ao) 
+		{
+			o.startBracket();			
+		}
+	}
 }
+
